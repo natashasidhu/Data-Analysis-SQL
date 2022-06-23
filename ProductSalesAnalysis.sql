23c6712eac2576a77e3e2ec7849bd0197f8b0a6d
natashasidhu <107374535+natashasidhu@users.noreply.github.com>
2022-06-23 19:34:02 -0400

    Update ProductSalesAnalysis.sql

--- ProductSalesAnalysis.sql
+++ ProductSalesAnalysis.sql
@@ -1,1 +1,75 @@
+--Product Sales Analysis I |
+--Sales table:
++---------+------------+------+----------+-------+
+| sale_id | product_id | year | quantity | price |
++---------+------------+------+----------+-------+
+| 1       | 100        | 2008 | 10       | 5000  |
+| 2       | 100        | 2009 | 12       | 5000  |
+| 7       | 200        | 2011 | 15       | 9000  |
++---------+------------+------+----------+-------+
 
+--Product table:
++------------+--------------+
+| product_id | product_name |
++------------+--------------+
+| 100        | Nokia        |
+| 200        | Apple        |
+| 300        | Samsung      |
++------------+--------------+
+
+--Write an SQL query that reports all product names of the products in the Sales table along with their selling year and price.
+
+--solution:
+
+SELECT product_name, year, price
+FROM Sales JOIN Product
+ON Product.product_id = Sales.product_id
+
+--Result table:
++--------------+-------+-------+
+| product_name | year  | price |
++--------------+-------+-------+
+| Nokia        | 2008  | 5000  |
+| Nokia        | 2009  | 5000  |
+| Apple        | 2011  | 9000  |
++--------------+-------+-------+
+
+--Product Sales Analysis II | 
+
+--Write an SQL query that reports the total quantity sold for every product id.
+
+--solution:
+SELECT product_id, sum(quantity) AS total_quantity
+FROM Sales
+GROUP BY product_id;
+
+--Result table:
++--------------+----------------+
+| product_id   | total_quantity |
++--------------+----------------+
+| 100          | 22             |
+| 200          | 15             |
++--------------+----------------+
+
+--Product Sales Analysis III | 
+--Write an SQL query that selects the product id, year, quantity, and price for the first year of every product sold.
+
+--solution:
+
+SELECT
+    product_id,
+    year first_year,
+    quantity,
+    price
+FROM Sales
+WHERE (product_id, year) IN (SELECT product_id, MIN(year)
+                             FROM Sales
+                             GROUP BY product_id)
+                             
+ --Result table:
++------------+------------+----------+-------+
+| product_id | first_year | quantity | price |
++------------+------------+----------+-------+
+| 100        | 2008       | 10       | 5000  |
+| 200        | 2011       | 15       | 9000  |
++------------+------------+----------+-------+                            
