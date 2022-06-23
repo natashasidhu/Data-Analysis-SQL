0e15dd19e9f0cf1c23a27a458945c16a18ce03c7
natashasidhu <107374535+natashasidhu@users.noreply.github.com>
2022-06-23 19:49:01 -0400

    Update questionspart1.sql

--- questionspart1.sql
+++ questionspart1.sql
@@ -125,6 +125,32 @@ HAVING count(DISTINCT product_key) = (
     SELECT count(1)
     FROM Product)
     
---    
+--  Active Businesses | Medium |
+--Write an SQL query to find all active businesses.
+--An active business is a business that has more than one event type with occurences greater than the average occurences of that event type among all businesses.
+--Events table:
++-------------+------------+------------+
+| business_id | event_type | occurences |
++-------------+------------+------------+
+| 1           | reviews    | 7          |
+| 3           | reviews    | 3          |
+| 1           | ads        | 11         |
+| 2           | ads        | 7          |
+| 3           | ads        | 6          |
+| 1           | page views | 3          |
+| 2           | page views | 12         |
++-------------+------------+------------+
 
+--solution:
+SELECT business_id
+FROM (SELECT a.business_id, a.event_type, a.occurences, b.event_avg  -- sub 2
+      FROM Events a LEFT JOIN
+        (SELECT event_type, AVG(occurences) event_avg   -- sub 1
+         FROM Events
+         GROUP BY event_type) b ON
+      a.event_type = b.event_type) tmp
+WHERE occurences > event_avg
+GROUP BY business_id
+HAVING COUNT(event_type) > 1
 
+--
