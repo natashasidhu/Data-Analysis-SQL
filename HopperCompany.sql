9c052377dfa74b0676e11b2cdcf4d316d99d34a3
natashasidhu <107374535+natashasidhu@users.noreply.github.com>
2022-06-23 21:26:45 -0400

    Create HopperCompany.sql

---
+++ HopperCompany.sql
@@ -0,0 +1,93 @@
+HARD QUERIES
+
+--Hopper Company Queries I | Hard |
+--Drivers table:
++-----------+------------+
+| driver_id | join_date  |
++-----------+------------+
+| 10        | 2019-12-10 |
+| 8         | 2020-1-13  |
+| 5         | 2020-2-16  |
+| 7         | 2020-3-8   |
+| 4         | 2020-5-17  |
+| 1         | 2020-10-24 |
+| 6         | 2021-1-5   |
++-----------+------------+
+--Rides table:
++---------+---------+--------------+
+| ride_id | user_id | requested_at |
++---------+---------+--------------+
+| 6       | 75      | 2019-12-9    |
+| 1       | 54      | 2020-2-9     |
+| 10      | 63      | 2020-3-4     |
+| 19      | 39      | 2020-4-6     |
+| 3       | 41      | 2020-6-3     |
+| 13      | 52      | 2020-6-22    |
+| 7       | 69      | 2020-7-16    |
+| 17      | 70      | 2020-8-25    |
+| 20      | 81      | 2020-11-2    |
+| 5       | 57      | 2020-11-9    |
+| 2       | 42      | 2020-12-9    |
+| 11      | 68      | 2021-1-11    |
+| 15      | 32      | 2021-1-17    |
+| 12      | 11      | 2021-1-19    |
+| 14      | 18      | 2021-1-27    |
++---------+---------+--------------+
+--AcceptedRides table:
++---------+-----------+---------------+---------------+
+| ride_id | driver_id | ride_distance | ride_duration |
++---------+-----------+---------------+---------------+
+| 10      | 10        | 63            | 38            |
+| 13      | 10        | 73            | 96            |
+| 7       | 8         | 100           | 28            |
+| 17      | 7         | 119           | 68            |
+| 20      | 1         | 121           | 92            |
+| 5       | 7         | 42            | 101           |
+| 2       | 4         | 6             | 38            |
+| 11      | 8         | 37            | 43            |
+| 15      | 8         | 108           | 82            |
+| 12      | 8         | 38            | 34            |
+| 14      | 1         | 90            | 74            |
++---------+-----------+---------------+---------------+
+
+--Write an SQL query to report the following statistics for each month of 2020:
+
+--The number of drivers currently with the Hopper company by the end of the month (active_drivers). 
+--The number of accepted rides in that month (accepted_rides). Return the result table ordered by month in ascending order, 
+--where month is the month’s number (January is 1, February is 2, etc.).
+
+solution:
+WITH RECURSIVE t1 AS (SELECT 1 AS month UNION ALL SELECT month+1 FROM t1 WHERE month<12)
+
+
+SELECT t1.month, COUNT(DISTINCT d.driver_id) as active_drivers, COUNT(DISTINCT v.ride_id) AS accepted_rides
+ FROM t1 LEFT JOIN
+(SELECT driver_id, (CASE WHEN YEAR(join_date)<2020 THEN 1 ELSE MONTH(join_date) END) AS drivermonth FROM drivers WHERE YEAR(join_date)<=2020) d
+ ON d.drivermonth<=t1.month
+ 
+LEFT JOIN
+(SELECT a.ride_id, MONTH(r.requested_at) as ridemonth FROM acceptedrides a JOIN rides r
+ ON a.ride_id=r.ride_id WHERE YEAR(r.requested_at)=2020) v
+ 
+ ON t1.month=v.ridemonth
+ 
+ GROUP BY t1.month
+ORDER BY t1.month
+
+--Result table:
++-------+----------------+----------------+
+| month | active_drivers | accepted_rides |
++-------+----------------+----------------+
+| 1     | 2              | 0              |
+| 2     | 3              | 0              |
+| 3     | 4              | 1              |
+| 4     | 4              | 0              |
+| 5     | 5              | 0              |
+| 6     | 5              | 1              |
+| 7     | 5              | 1              |
+| 8     | 5              | 1              |
+| 9     | 5              | 0              |
+| 10    | 6              | 0              |
+| 11    | 6              | 2              |
+| 12    | 6              | 1              |
++-------+----------------+----------------+
