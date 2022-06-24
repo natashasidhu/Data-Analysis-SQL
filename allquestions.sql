3b01f4c694048ffac49d587aa9c936cad6b6e19a
natashasidhu <107374535+natashasidhu@users.noreply.github.com>
2022-06-23 21:21:07 -0400

    Update allquestions.sql

--- allquestions.sql
+++ allquestions.sql
@@ -125,7 +125,209 @@ JOIN Weather y
 ON DATEDIFF(t.recordDate, y.recordDate) = 1 AND
 t.temperature > y.temperature;
 
---question
+--question Customer Order Frequency |
+--Customers
++--------------+-----------+-------------+
+| customer_id  | name      | country     |
++--------------+-----------+-------------+
+| 1            | Winston   | USA         |
+| 2            | Jonathan  | Peru        |
+| 3            | Moustafa  | Egypt       |
++--------------+-----------+-------------+
+--Product
++--------------+-------------+-------------+
+| product_id   | description | price       |
++--------------+-------------+-------------+
+| 10           | LC Phone    | 300         |
+| 20           | LC T-Shirt  | 10          |
+| 30           | LC Book     | 45          |
+| 40           | LC Keychain | 2           |
++--------------+-------------+-------------+
+--Orders
++--------------+-------------+-------------+-------------+-----------+
+| order_id     | customer_id | product_id  | order_date  | quantity  |
++--------------+-------------+-------------+-------------+-----------+
+| 1            | 1           | 10          | 2020-06-10  | 1         |
+| 2            | 1           | 20          | 2020-07-01  | 1         |
+| 3            | 1           | 30          | 2020-07-08  | 2         |
+| 4            | 2           | 10          | 2020-06-15  | 2         |
+| 5            | 2           | 40          | 2020-07-01  | 10        |
+| 6            | 3           | 20          | 2020-06-24  | 2         |
+| 7            | 3           | 30          | 2020-06-25  | 2         |
+| 9            | 3           | 30          | 2020-05-08  | 3         |
++--------------+-------------+-------------+-------------+-----------+
+
+--Write an SQL query to report the customerid and customername of customers who have spent at least $100 in each month of June and July 2020.
+
+--solution:
+SELECT customer_id, name
+FROM
+(
+    SELECT o.customer_id, c.name,
+        sum(CASE WHEN left(o.order_date,7) = '2020-06' THEN p.price * o.quantity END) AS JuneSpend,
+        sum(CASE WHEN left(o.order_date,7) = '2020-07' THEN p.price * o.quantity END) AS JulySpend
+    FROM Orders o
+    LEFT JOIN Customers c ON o.customer_id = c.customer_id
+    lEFT JOIN Product p ON o.product_id = p.product_id
+    GROUP BY o.customer_id
+    HAVING JuneSpend >= 100 AND JulySpend >= 100
+) AS temp
+
+--The Most Recent Orders for Each Product | Medium |
+--Customers
++-------------+-----------+
+| customer_id | name      |
++-------------+-----------+
+| 1           | Winston   |
+| 2           | Jonathan  |
+| 3           | Annabelle |
+| 4           | Marwan    |
+| 5           | Khaled    |
++-------------+-----------+
+--Orders
++----------+------------+-------------+------------+
+| order_id | order_date | customer_id | product_id |
++----------+------------+-------------+------------+
+| 1        | 2020-07-31 | 1           | 1          |
+| 2        | 2020-07-30 | 2           | 2          |
+| 3        | 2020-08-29 | 3           | 3          |
+| 4        | 2020-07-29 | 4           | 1          |
+| 5        | 2020-06-10 | 1           | 2          |
+| 6        | 2020-08-01 | 2           | 1          |
+| 7        | 2020-08-01 | 3           | 1          |
+| 8        | 2020-08-03 | 1           | 2          |
+| 9        | 2020-08-07 | 2           | 3          |
+| 10       | 2020-07-15 | 1           | 2          |
++----------+------------+-------------+------------+
+--Products
++------------+--------------+-------+
+| product_id | product_name | price |
++------------+--------------+-------+
+| 1          | keyboard     | 120   |
+| 2          | mouse        | 80    |
+| 3          | screen       | 600   |
+| 4          | hard disk    | 450   |
++------------+--------------+-------+
+
+--Write an SQL query to find the most recent order(s) of each product.
+--solution:
+
+SELECT p.product_name, o.product_id, o.order_id, o.order_date
+FROM(
+    SELECT product_id, order_id, order_date,
+    RANK() OVER(PARTITION BY product_id ORDER BY order_date DESC) AS seq
+    FROM orders
+) o
+LEFT JOIN products p
+    ON o.product_id = p.product_id
+WHERE o.seq = 1
+ORDER BY 1,2,3
+
+--Result table:
++--------------+------------+----------+------------+
+| product_name | product_id | order_id | order_date |
++--------------+------------+----------+------------+
+| keyboard     | 1          | 6        | 2020-08-01 |
+| keyboard     | 1          | 7        | 2020-08-01 |
+| mouse        | 2          | 8        | 2020-08-03 |
+| screen       | 3          | 3        | 2020-08-29 |
++--------------+------------+----------+------------+
+
+--QUESTION|Unique Orders and Customers Per Month | Easy |
+--Orders
++----------+------------+-------------+------------+
+| order_id | order_date | customer_id | invoice    |
++----------+------------+-------------+------------+
+| 1        | 2020-09-15 | 1           | 30         |
+| 2        | 2020-09-17 | 2           | 90         |
+| 3        | 2020-10-06 | 3           | 20         |
+| 4        | 2020-10-20 | 3           | 21         |
+| 5        | 2020-11-10 | 1           | 10         |
+| 6        | 2020-11-21 | 2           | 15         |
+| 7        | 2020-12-01 | 4           | 55         |
+| 8        | 2020-12-03 | 4           | 77         |
+| 9        | 2021-01-07 | 3           | 31         |
+| 10       | 2021-01-15 | 2           | 20         |
++----------+------------+-------------+------------+
+--Write an SQL query to find the number of unique orders and the number of unique users with invoices > $20 for each different month.
+--Solution:
+SELECT LEFT(order_date, 7) AS month, COUNT(DISTINCT order_id) AS order_count,
+	COUNT(DISTINCT customer_id) AS customer_count
+FROM orders
+WHERE invoice > 20
+GROUP BY month
+
+--Result table:
++---------+-------------+----------------+
+| month   | order_count | customer_count |
++---------+-------------+----------------+
+| 2020-09 | 2           | 2              |
+| 2020-10 | 1           | 1              |
+| 2020-12 | 2           | 1              |
+| 2021-01 | 1           | 1              |
++---------+-------------+----------------+
+
+--QUESTION|Customer Who Visited but Did Not Make Any Transactions | Easy | 
+--Visits
++----------+-------------+
+| visit_id | customer_id |
++----------+-------------+
+| 1        | 23          |
+| 2        | 9           |
+| 4        | 30          |
+| 5        | 54          |
+| 6        | 96          |
+| 7        | 54          |
+| 8        | 54          |
++----------+-------------+
+--Transactions
++----------------+----------+--------+
+| transaction_id | visit_id | amount |
++----------------+----------+--------+
+| 2              | 5        | 310    |
+| 3              | 5        | 300    |
+| 9              | 5        | 200    |
+| 12             | 1        | 910    |
+| 13             | 2        | 970    |
++----------------+----------+--------+
+
+--Write an SQL query to find the IDs of the users who visited without making any transactions and the number of times they made these types of visits.
+--solution:
+SELECT a.customer_id, COUNT(a.visit_id) AS count_no_trans FROM Visits AS a
+LEFT JOIN Transactions AS b
+ON a.visit_id = b.visit_id
+WHERE b.transaction_id IS NULL
+GROUP BY a.customer_id;
+
+--Sellers With No Sales | Easy |
+--Orders table:
++-------------+------------+--------------+-------------+-------------+
+| order_id    | sale_date  | order_cost   | customer_id | seller_id   |
++-------------+------------+--------------+-------------+-------------+
+| 1           | 2020-03-01 | 1500         | 101         | 1           |
+| 2           | 2020-05-25 | 2400         | 102         | 2           |
+| 3           | 2019-05-25 | 800          | 101         | 3           |
+| 4           | 2020-09-13 | 1000         | 103         | 2           |
+| 5           | 2019-02-11 | 700          | 101         | 2           |
++-------------+------------+--------------+-------------+-------------+
+--Seller table:
++-------------+-------------+
+| seller_id   | seller_name |
++-------------+-------------+
+| 1           | Daniel      |
+| 2           | Elizabeth   |
+| 3           | Frank       |
++-------------+-------------+
+
+--Write an SQL query to report the names of all sellers who did not make any sales in 2020.
+
+--solution:
+SELECT seller_name FROM Seller
+WHERE seller_id NOT IN (
+SELECT DISTINCT seller_id FROM Orders
+WHERE YEAR(sale_date)='2020'
+)
+ORDER BY seller_name;
 
 
 
